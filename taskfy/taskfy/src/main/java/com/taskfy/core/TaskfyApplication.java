1e845c5c379eefbdebc7ff04357081ffaa539410
Mayke <mayke000000000@gmail.com>
2025-02-07 10:43:26 -0300

    chore: cria a estrutura inicial do projeto Spring

---
+++ taskfy/taskfy/src/main/java/com/taskfy/core/TaskfyApplication.java
@@ -0,0 +1,13 @@
+package com.taskfy.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskfyApplication.class, args);
+	}
+
+}
