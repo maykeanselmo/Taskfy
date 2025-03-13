f90d59f7c07624d1ed23ca47cce2d915d94b7ab4
Mayke <mayke000000000@gmail.com>
2025-03-13 11:05:37 -0300

    feat: Validações de campos

--- api/taskfy/src/main/java/com/taskfy/core/domain/Users.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/Users.java
@@ -16,18 +16,33 @@ import java.io.Serializable;
 @Table(name = "users")
 public class Users implements Serializable {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Column(name = "username", nullable = false, unique = true)
+    @Size(min = 3, max = 30, message = "O nome de usuário deve ter entre 3 e 30 caracteres")
     private String username;
+
+    @NotBlank
+    @Email(message = "Formato inválido de email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "active", nullable = false)
     private boolean active;
 
     @PrePersist
     @PreUpdate
     private void formatFields() {
         this.email = this.email.toLowerCase();
+        this.username = this.username.toLowerCase();
     }
-
 }
