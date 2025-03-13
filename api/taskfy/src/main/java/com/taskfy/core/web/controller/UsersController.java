f3e6d95b307ec97a2b6c7edf27451bdc5b2d58a8
Mayke <mayke000000000@gmail.com>
2025-03-13 12:48:54 -0300

    feat:  endpoit de criação de user

--- api/taskfy/src/main/java/com/taskfy/core/web/controller/UsersController.java
+++ api/taskfy/src/main/java/com/taskfy/core/web/controller/UsersController.java
@@ -1,7 +1,16 @@
 package com.taskfy.core.web.controller;
 
+import com.taskfy.core.domain.users.model.Users;
 import com.taskfy.core.domain.users.service.UsersService;
+import com.taskfy.core.web.dto.UserCreateDto;
+import com.taskfy.core.web.dto.UserResponseDto;
+import com.taskfy.core.web.dto.mapper.UsersMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class UsersController {
     private final UsersService usersService;
+
+    @PostMapping("/users")
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto dto){
+        Users user = usersService.createUser(UsersMapper.toUser(dto));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(UsersMapper.toDto(user));
+    }
+
 }
