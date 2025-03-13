aa790e1422503a4b0df22a7138c49fb12c3929f8
Mayke <mayke000000000@gmail.com>
2025-03-13 11:00:13 -0300

    feat: Entidade users

---
+++ api/taskfy/src/main/java/com/taskfy/core/domain/Users.java
@@ -0,0 +1,34 @@
+package com.taskfy.core.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Table(name = "users")
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean active;
+
+    @PrePersist
+    @PreUpdate
+    private void formatFields() {
+        this.email = this.email.toLowerCase();
+    }
+
+}
