8b5e4c2e459b46e0a019e8d2e8c0088933f7da3d
Mayke <mayke000000000@gmail.com>
2025-03-13 12:45:39 -0300

    feat: método create user no service

--- api/taskfy/src/main/java/com/taskfy/core/domain/users/service/UsersService.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/users/service/UsersService.java
@@ -1,7 +1,13 @@
 package com.taskfy.core.domain.users.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.taskfy.core.domain.users.exeption.ErrorNotificationException;
+import com.taskfy.core.domain.users.exeption.UserAlreadyExistsException;
+import com.taskfy.core.domain.users.model.Users;
 import com.taskfy.core.domain.users.repository.UsersRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +15,16 @@ import org.springframework.stereotype.Service;
 public class UsersService {
     private final UsersRepository usersRepository;
 
+    @Transactional
+    public Users createUser(Users user){
+        try{
+            return usersRepository.save(user);
+        } catch (DataIntegrityViolationException e){
+            throw new UserAlreadyExistsException("Já existe usuário cadastrado com esse email ou username");
+        } catch (Exception e){
+            throw  new RuntimeException("Erro ao criar usuário. Tente novamente mais tarde.");
+        }
+
+    }
 
 }
