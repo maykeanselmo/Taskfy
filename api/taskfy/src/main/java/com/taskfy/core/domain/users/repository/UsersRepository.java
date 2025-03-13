b580d7e4733b462f7d733a45e72f64ceabdbf447
Mayke <mayke000000000@gmail.com>
2025-03-13 11:19:36 -0300

    feat: Criação de Repositório de users

---
+++ api/taskfy/src/main/java/com/taskfy/core/domain/users/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.taskfy.core.domain.users.repository;
+
+import com.taskfy.core.domain.users.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
