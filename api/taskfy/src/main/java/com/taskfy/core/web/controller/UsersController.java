11f4c2938caaec0df232a7c320990e24239d5894
Mayke <mayke000000000@gmail.com>
2025-03-15 21:38:42 -0300

    feat: endpoint GetById

--- api/taskfy/src/main/java/com/taskfy/core/web/controller/UsersController.java
+++ api/taskfy/src/main/java/com/taskfy/core/web/controller/UsersController.java
@@ -2,20 +2,22 @@ package com.taskfy.core.web.controller;
 
 import com.taskfy.core.domain.users.model.Users;
 import com.taskfy.core.domain.users.service.UsersService;
+import com.taskfy.core.web.dto.UpdatePasswordDto;
+import com.taskfy.core.web.dto.UpdateUserDto;
 import com.taskfy.core.web.dto.UserCreateDto;
 import com.taskfy.core.web.dto.UserResponseDto;
 import com.taskfy.core.web.dto.mapper.UsersMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/v1")
 public class UsersController {
     private final UsersService usersService;
@@ -23,9 +25,18 @@ public class UsersController {
     @PostMapping("/users")
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto dto){
         Users user = usersService.createUser(UsersMapper.toUser(dto));
+        log.info("Usuario criado com sucesso");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(UsersMapper.toDto(user));
     }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable("id") Long id) {
+        Users user = usersService.getUserById(id);
+        log.info("Usuario encontrado com sucesso");
+
+        UserResponseDto userResponseDto = UsersMapper.toDto(user);
+        return ResponseEntity.ok(userResponseDto);
+    }
 }
