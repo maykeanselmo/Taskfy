7f216f29555989784c3fe9ca055f2809cd3ac983
Mayke <mayke000000000@gmail.com>
2025-03-16 16:22:52 -0300

    refac: tipo de retorno de findById

--- api/taskfy/src/main/java/com/taskfy/core/domain/users/repository/UsersRepository.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/users/repository/UsersRepository.java
@@ -2,11 +2,12 @@ package com.taskfy.core.domain.users.repository;
 
 import com.taskfy.core.domain.users.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    Users findByEmail(String email);
+    UserDetails findByEmail(String email);
 }
