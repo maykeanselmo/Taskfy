e873641fed6fb5704039b9b5030b00e93b570706
unitynathan <dmoraesabreu_nathan404@outlook.com>
2025-03-22 12:52:12 -0300

    Feat: Provedor de contexto de pastas

--- taskfy_web_app/src/index.js
+++ taskfy_web_app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FolderProvider } from "./context/FolderContext";
 
 import { RecoilRoot } from 'recoil';
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <RecoilRoot> {/* Envolva o App com RecoilRoot */}
-        <App />
+      <FolderProvider>
+      <App />
+      </FolderProvider>
       </RecoilRoot>
   </React.StrictMode>
 );
