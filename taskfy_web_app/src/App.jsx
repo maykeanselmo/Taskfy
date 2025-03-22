e30419f83aca33b6b4320af49788b7caf4691688
unitynathan <dmoraesabreu_nathan404@outlook.com>
2025-03-22 12:51:19 -0300

    Refact: extensão de arquivo JS para JSX

---
+++ taskfy_web_app/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/login';
+import Home from './pages/home';
+import About from './pages/about';
+import Editor from './pages/editor';
+import Settings from './pages/settings';
+import Notes from './pages/notes';
+import { t } from './utils/translations';
+import { FolderProvider } from "./context/FolderContext";
+
+function App() {
+  return (
+    <FolderProvider>
+      <Router>
+        <div className="App">
+          {/* Menu de navegação */}
+          <nav>
+            <Link to="/login">{t("login")}</Link>
+            <Link to="/">{t("home")}</Link>
+            <Link to="/about">{t("about")}</Link>
+            <Link to="/editor">{t("editor")}</Link>
+            <Link to="/settings">{t("settings")}</Link>
+            <Link to="/notes">{t("notes")}</Link>
+          </nav>
+
+          {/* Rotas */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/notes" element={<Notes />} />
+          </Routes>
+        </div>
+      </Router>
+      </FolderProvider>
+  );
+}
+
+export default App;
