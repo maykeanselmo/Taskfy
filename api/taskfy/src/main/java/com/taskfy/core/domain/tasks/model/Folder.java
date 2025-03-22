d3c8a5228cc498acc6135066df2e69f76e2a2de6
Mayke <mayke000000000@gmail.com>
2025-03-22 20:12:05 -0300

    feat: Validações de Folder

--- api/taskfy/src/main/java/com/taskfy/core/domain/tasks/model/Folder.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/tasks/model/Folder.java
@@ -1,6 +1,10 @@
 package com.taskfy.core.domain.tasks.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -19,10 +23,24 @@ public class Folder {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotNull(message = "User ID cannot be null")
+    @Column(name = "user_id", nullable = false)
     private Long userId;
+
+    @Column(name = "parent_id")
     private Long parentId;
+
+    @NotBlank(message = "Folder name cannot be empty")
+    @Size(max = 255, message = "Folder name cannot be longer than 255 characters")
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @PastOrPresent(message = "Creation date must be in the past or present")
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @PastOrPresent(message = "Update date must be in the past or present")
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @PrePersist
@@ -37,5 +55,4 @@ public class Folder {
     public void preUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
-
 }
