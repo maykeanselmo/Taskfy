93b2c80b2d4fc634d99c48f3b5b8047a195c89b0
Joao Vitor <joaovitorshenriques@gmail.com>
2025-03-22 19:40:37 -0300

    feat: adicionando botao com drawer para melhorar visualizacao do menu

--- taskfy_web_app/src/App.js
+++ taskfy_web_app/src/App.js
@@ -1,29 +1,124 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import LoginPage from './pages/login';
-import Home from './pages/home';
-import About from './pages/about';
-import Editor from './pages/editor';
-import Settings from './pages/settings';
-import Notes from './pages/notes';
-import { t } from './utils/translations';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import IconButton from "@mui/material/IconButton";
+import "./App.css";
+import LoginPage from "./pages/login";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Editor from "./pages/editor";
+import Settings from "./pages/settings";
+import Notes from "./pages/notes";
+import { t } from "./utils/translations";
 
-function App() {
+export default function App() {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => {
+    setDrawerOpen(open);
+  };
+
   return (
-      <Router>
-        <div className="App">
-          {/* Menu de navegação */}
-          <nav>
-            <Link to="/login">{t("login")}</Link>
-            <Link to="/">{t("home")}</Link>
-            <Link to="/about">{t("about")}</Link>
-            <Link to="/editor">{t("editor")}</Link>
-            <Link to="/settings">{t("settings")}</Link>
-            <Link to="/notes">{t("notes")}</Link>
-          </nav>
+    <Router>
+      <div className="App">
+        {/* Botão de menu que desaparece quando o Drawer está aberto */}
+        {!drawerOpen && (
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => toggleDrawer(true)}
+            sx={{
+              position: "absolute",
+              left: 10,
+              top: 10,
+              zIndex: 1300, // Garante que o botão fique acima de outros elementos
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
 
-          {/* Rotas */}
+        {/* Drawer Temporário */}
+        <Drawer
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: 240,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="temporary"
+          anchor="left"
+          open={drawerOpen}
+          onClose={() => toggleDrawer(false)} // Fecha apenas quando clicar fora
+        >
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/login">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("login")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("home")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/about">
+                <ListItemIcon>
+                  <MailIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("about")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/editor">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("editor")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/settings">
+                <ListItemIcon>
+                  <MailIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("settings")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/notes">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={t("notes")} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+
+        {/* Conteúdo da aplicação */}
+        <Box sx={{ padding: 2 }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
@@ -32,10 +127,8 @@ function App() {
             <Route path="/settings" element={<Settings />} />
             <Route path="/notes" element={<Notes />} />
           </Routes>
-        </div>
-      </Router>
+        </Box>
+      </div>
+    </Router>
   );
 }
-
-export default App;
-
