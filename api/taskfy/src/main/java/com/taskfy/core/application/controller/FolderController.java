466b6034aac45e42c8465258fdd9755adcfbc448
Mayke <mayke000000000@gmail.com>
2025-03-22 22:00:36 -0300

    feat: criação de controller do domain task

---
+++ api/taskfy/src/main/java/com/taskfy/core/application/controller/FolderController.java
@@ -0,0 +1,15 @@
+package com.taskfy.core.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/v1")
+public class FolderController {
+
+
+}
