57e3535f4777a612067019967b01cbfa9aa75a62
Mayke <mayke000000000@gmail.com>
2025-03-23 14:31:46 -0300

    feat: serviço de retorno de task por id

--- api/taskfy/src/main/java/com/taskfy/core/domain/tasks/service/TaskService.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/tasks/service/TaskService.java
@@ -1,12 +1,12 @@
 package com.taskfy.core.domain.tasks.service;
 
 import com.taskfy.core.domain.tasks.exception.FolderNotFoundException;
-import com.taskfy.core.domain.tasks.exception.TaskCreationException;
 import com.taskfy.core.domain.tasks.model.Folder;
 import com.taskfy.core.domain.tasks.model.Tasks;
 import com.taskfy.core.domain.tasks.repository.FolderRepository;
 import com.taskfy.core.domain.tasks.repository.TaskRepository;
 import com.taskfy.core.domain.users.exeption.UserAlreadyExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -33,5 +33,12 @@ public class TaskService {
         } catch (Exception e) {
             throw new RuntimeException("Erro ao criar tarefa. Tente novamente mais tarde.");
         }
+    }
+
+    @Transactional
+    public Tasks getTaskById(Long id){
+        return taskRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Nenhuma tarefa foi encontrado com este id: " + id)
+        );
     }
 }
