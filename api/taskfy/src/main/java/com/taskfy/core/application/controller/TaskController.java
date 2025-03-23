c4904f25ccc173f610a7c090bad9c8314dd5e4a6
Mayke <mayke000000000@gmail.com>
2025-03-23 14:14:46 -0300

    feat:Endpoint de criação de task

--- api/taskfy/src/main/java/com/taskfy/core/application/controller/TaskController.java
+++ api/taskfy/src/main/java/com/taskfy/core/application/controller/TaskController.java
@@ -1,7 +1,17 @@
 package com.taskfy.core.application.controller;
 
+import com.taskfy.core.application.dto.mapper.TaskMapper;
+import com.taskfy.core.application.dto.request.TaskCreateDTO;
+import com.taskfy.core.application.dto.response.TaskResponseDTO;
+import com.taskfy.core.domain.tasks.model.Tasks;
+import com.taskfy.core.domain.tasks.service.TaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/v1")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping("/tasks")
+    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskCreateDTO dto){
+        Tasks task = taskService.createTask(TaskMapper.toTask(dto));
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(TaskMapper.toResponseDto(task));
+    }
 }
--- api/taskfy/src/main/java/com/taskfy/core/domain/tasks/service/TaskService.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/tasks/service/TaskService.java
@@ -1,7 +1,37 @@
 package com.taskfy.core.domain.tasks.service;
 
+import com.taskfy.core.domain.tasks.exception.FolderNotFoundException;
+import com.taskfy.core.domain.tasks.exception.TaskCreationException;
+import com.taskfy.core.domain.tasks.model.Folder;
+import com.taskfy.core.domain.tasks.model.Tasks;
+import com.taskfy.core.domain.tasks.repository.FolderRepository;
+import com.taskfy.core.domain.tasks.repository.TaskRepository;
+import com.taskfy.core.domain.users.exeption.UserAlreadyExistsException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+    private final FolderRepository folderRepository;
+
+    @Transactional
+    public Tasks createTask(Tasks task) {
+
+        Folder folder = folderRepository.findById(task.getFolder().getId())
+                .orElseThrow(() -> new FolderNotFoundException("Folder not found with id: " + task.getFolder().getId()));
+
+        task.setFolder(folder);
+
+        try {
+            return taskRepository.save(task);
+        } catch (DataIntegrityViolationException e) {
+            throw new UserAlreadyExistsException("Task creation failed due to data integrity violation.");
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao criar tarefa. Tente novamente mais tarde.");
+        }
+    }
 }
