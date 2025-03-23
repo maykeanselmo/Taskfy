4079af1428c91a23bd125c8b03769039c6ac00f1
Kyamel <lcamelo200@gmail.com>
2025-03-23 00:04:40 -0300

    fix: Bugs ao criar classe para abstrair o indexd

--- taskfy_web_app/src/services/indexdb.js
+++ taskfy_web_app/src/services/indexdb.js
@@ -1,4 +1,5 @@
 const openDatabase = () => {
+<<<<<<< HEAD
   return new Promise((resolve, reject) => {
     const request = indexedDB.open('taskfy', 1);
 
@@ -90,6 +91,59 @@ export const getUserByUsername = async (username) => {
   }
 };
 
+=======
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open('taskfy', 1);
+
+      request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        // Criar object stores (tabelas) se não existirem
+        if (!db.objectStoreNames.contains('users')) {
+          db.createObjectStore('users', { keyPath: 'id' }); // O id será a chave primária
+        }
+        if (!db.objectStoreNames.contains('folders')) {
+          db.createObjectStore('folders', { keyPath: 'id' });
+        }
+        if (!db.objectStoreNames.contains('tasks')) {
+          db.createObjectStore('tasks', { keyPath: 'id' });
+        }
+      };
+
+      request.onsuccess = (event) => {
+        resolve(event.target.result);
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
+    });
+  };
+
+
+export  const saveToIndexedDB = (storeName, data) => {
+    return new Promise(async (resolve, reject) => {
+        const db = await openDatabase();
+        const transaction = db.transaction(storeName, 'readwrite');
+        const store = transaction.objectStore(storeName);
+
+        const request = store.put(data); // Usando put para atualizar ou adicionar um novo item
+        request.onsuccess = () => resolve();
+        request.onerror = (error) => reject(error);
+    });
+};
+
+export  const getFromIndexedDB = (storeName, key) => {
+    return new Promise(async (resolve, reject) => {
+      const db = await openDatabase();
+      const transaction = db.transaction(storeName, 'readonly');
+      const store = transaction.objectStore(storeName);
+
+      const request = store.get(key);
+      request.onsuccess = (event) => resolve(event.target.result);
+      request.onerror = (error) => reject(error);
+    });
+  };
+>>>>>>> 1a21ff2 (fix: Bugs ao criar classe para abstrair o indexd)
 
 export const deleteFromIndexedDB = (storeName, key) => {
     return new Promise(async (resolve, reject) => {
