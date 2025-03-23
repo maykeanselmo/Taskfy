3ca5310ec5faa57ffb2af0f119cad81b38f070f5
Mayke <mayke000000000@gmail.com>
2025-03-23 11:24:34 -0300

    feat: implementação de  endpoints de getRootFolder, gtSubFolders e getFodlerByUserId

--- api/taskfy/src/main/java/com/taskfy/core/application/controller/FolderController.java
+++ api/taskfy/src/main/java/com/taskfy/core/application/controller/FolderController.java
@@ -3,7 +3,6 @@ package com.taskfy.core.application.controller;
 import com.taskfy.core.application.dto.mapper.FolderMapper;
 import com.taskfy.core.application.dto.request.FolderCreateDTO;
 import com.taskfy.core.application.dto.response.FolderResponseDTO;
-import com.taskfy.core.domain.tasks.exception.FolderNotFoundException;
 import com.taskfy.core.domain.tasks.model.Folder;
 import com.taskfy.core.domain.tasks.service.FolderService;
 import jakarta.validation.Valid;
@@ -12,6 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,7 +32,7 @@ public class FolderController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FolderResponseDTO> createFolder(@PathVariable("id") Long id) {
+    public ResponseEntity<FolderResponseDTO> getFolderById(@PathVariable("id") Long id) {
         Folder createdFolder = folderService.getFolderByID(id);
         return ResponseEntity.ok(FolderMapper.toDto(createdFolder));
     }
@@ -41,5 +43,37 @@ public class FolderController {
             return ResponseEntity.noContent().build();
 
     }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<FolderResponseDTO>> getFoldersByUser(@PathVariable Long userId) {
+        List<Folder> folders = folderService.getFoldersByUserId(userId);
+        List<FolderResponseDTO> response = folders.stream()
+                .map(FolderMapper::toDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/user/{userId}/root")
+    public ResponseEntity<List<FolderResponseDTO>> getRootFoldersByUser(@PathVariable Long userId) {
+        List<Folder> rootFolders = folderService.getRootFoldersByUserId(userId);
+
+        List<FolderResponseDTO> response = rootFolders.stream()
+                .map(FolderMapper::toDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/{id}/subfolders")
+    public ResponseEntity<List<FolderResponseDTO>> getSubFolders(@PathVariable("id") Long id) {
+        List<Folder> subFolders = folderService.getSubFoldersByFolderId(id);
+        List<FolderResponseDTO> response = subFolders.stream()
+                .map(FolderMapper::toDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(response);
+    }
+
+
 
 }
