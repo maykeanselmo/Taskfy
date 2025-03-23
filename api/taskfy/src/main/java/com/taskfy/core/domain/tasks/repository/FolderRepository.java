5dc348e539d5a6742cbca487943b064a4e8f1d08
Mayke <mayke000000000@gmail.com>
2025-03-23 11:43:42 -0300

    feat: novas consultas personalizadas do repository

--- api/taskfy/src/main/java/com/taskfy/core/domain/tasks/repository/FolderRepository.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/tasks/repository/FolderRepository.java
@@ -2,8 +2,18 @@ package com.taskfy.core.domain.tasks.repository;
 
 import com.taskfy.core.domain.tasks.model.Folder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findByUserId(Long userId);
+
+    @Query("SELECT f FROM Folder f WHERE f.parentFolder IS NULL AND f.user.id = :userId")
+    List<Folder> findRootFoldersByUserId(Long userId);
+
+    @Query("SELECT f FROM Folder f LEFT JOIN FETCH f.subFolders WHERE f.id = :id")
+    Folder findFolderWithSubFoldersById(Long id);
 }
