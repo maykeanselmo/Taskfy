be232433f8f1612974728abad4cd660f5b132c43
Mayke <mayke000000000@gmail.com>
2025-03-29 23:47:57 -0300

    refac: novos métodos no repository

--- api/taskfy/src/main/java/com/taskfy/core/domain/tasks/repository/TaskTagRepository.java
+++ api/taskfy/src/main/java/com/taskfy/core/domain/tasks/repository/TaskTagRepository.java
@@ -1,9 +1,17 @@
 package com.taskfy.core.domain.tasks.repository;
 
+import com.taskfy.core.domain.tasks.model.Tag;
 import com.taskfy.core.domain.tasks.model.TaskTag;
+import com.taskfy.core.domain.tasks.model.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TaskTagRepository extends JpaRepository<TaskTag, Long> {
+    boolean existsByTaskIdAndTagId(Long taskId, Long tagId);
+    Optional<TaskTag> findByTaskIdAndTagId(Long taskId, Long tagId);
+    List<TaskTag> findByTaskId(Long taskId);
 }
