dcea5d599d83b074109bf62d58a205285174b883
Kyamel <lcamelo200@gmail.com>
2025-03-31 15:10:37 -0300

    fix: importação dbService

--- taskfy_web_app/src/components/create_task_buttom.jsx
+++ taskfy_web_app/src/components/create_task_buttom.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import dbService from '../services/db_service';
+import { dbService }  from '../services/db_service';
 function CreateTaskButton() {
   const addTask = useCreateTask();
   const [loading, setLoading] = useState(false);
