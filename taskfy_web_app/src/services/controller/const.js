24560ce89d2c0503bf5a014347e107807412bacc
Kyamel <lcamelo200@gmail.com>
2025-04-03 18:03:03 -0300

    tentando concertar a build do backend

--- taskfy_web_app/src/services/controller/const.js
+++ taskfy_web_app/src/services/controller/const.js
@@ -1,10 +1,2 @@
 export const API_BASE_URL = 'http://localhost:8080';
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const VER = '/v1'
-=======
-export const VER = '/v1'
->>>>>>> 1b6479b (fear: Comeco implementar folder tree viewr no explorere)
-=======
-export const VER = '/v1'
->>>>>>> 5beb02fae14bd74639c3e92f8e4a101f1032a4f5
+export const VER = '/v1';
